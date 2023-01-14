eab2bc567eb6869517645739981fa5ee532c86f3
Matheus Costa <mathfernandesz@gmail.com>
2023-01-14 08:53:10 -0300

    CoyoteTimer and JumpBuffer

--- Entities/Player/Player.gd
+++ Entities/Player/Player.gd
@@ -8,10 +8,16 @@ extends CharacterBody2D
 
 
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var coyote_timer: Timer = $CoyoteTimer
+@onready var jump_buffer: Timer = $JumpBuffer
 
 
 var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+#---- JUMP
+var is_grounded: bool = true
 var is_jumping: bool = false
+var has_presssed_jump: bool = false
 
 
 func _physics_process(delta: float) -> void:
@@ -22,33 +28,41 @@ func _physics_process(delta: float) -> void:
 
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed('jump') and is_on_floor():
-		jump()
+	# JUMP
+	if event.is_action_pressed('jump'):
+		if is_on_floor() or not coyote_timer.is_stopped():
+			coyote_timer.stop()
+			jump()
+		else:
+			jump_buffer.start()
 
 
 func apply_gravity(delta: float) -> void:
-	if not is_on_floor():
+	if not is_on_floor() and coyote_timer.is_stopped():
 		velocity.y += gravity * delta
+	
+	if is_jumping and velocity.y >= 0:
+		is_jumping = false
+	
+	# --- JUMP BUFFER
+	if is_on_floor() and not jump_buffer.is_stopped():
+		jump_buffer.stop()
+		jump()
 
 
 func get_input() -> float:
 	return Input.get_axis("move_left", "move_right")
 
 
-func is_grounded() -> bool:
-	if is_on_floor():
-		is_jumping = false
-		return true
-	else:
-		return false
-
-
 func jump() -> void:
-	is_jumping = true
 	velocity.y = jump_velocity
+	is_jumping = true
+
 
 func move() -> void:
 	var direction: float = get_input()
+	var was_on_floor: bool =  is_on_floor()
+	
 	
 	if direction:
 		velocity.x = direction * move_speed
@@ -56,6 +70,14 @@ func move() -> void:
 		velocity.x = move_toward(velocity.x, 0, move_speed)
 	
 	move_and_slide()
+	
+	# --- COYOTE
+	if not is_on_floor() and was_on_floor and not is_jumping:
+		coyote_timer.start()
+		velocity.y = 0
+	
+	is_grounded = is_on_floor()
+	
 
 
 func animation() -> void:
