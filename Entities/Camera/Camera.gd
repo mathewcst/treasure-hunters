40cd745261e1c8a8367d4cff859c5b45d80a4ef9
Matheus Costa <mathfernandesz@gmail.com>
2023-01-14 09:15:25 -0300

    Camera test

--- Entities/Camera/Camera.gd
+++ Entities/Camera/Camera.gd
@@ -2,6 +2,7 @@ extends Camera2D
 
 @export var follow_target: Node2D
 
-func _process(_delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	assert(follow_target, "Missing Target to follow")
-	global_position = follow_target.global_position
+	var tween = create_tween().set_trans(Tween.TRANS_BOUNCE).set_ease(Tween.EASE_IN_OUT)
+	tween.tween_property(self, 'global_position', follow_target.global_position, delta)
